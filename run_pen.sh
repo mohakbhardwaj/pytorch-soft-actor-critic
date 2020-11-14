3270dc009cb0603a6444cc3edaf151f083939844
Mohak Bhardwaj <mohakb@nvidia.com>
2020-11-14 13:16:27 -0800

    peg config

--- run_pen.sh
+++ run_pen.sh
@@ -1,3 +1,3 @@
 # python main.py --env-name pen-v0 --num_steps 10000000 --eval_episodes 30 --eval_freq 40 --seed 0 --test_seed 999 --automatic_entropy_tuning True
 # python main.py --env-name pen-v0 --num_steps 10000000 --eval_episodes 30 --eval_freq 40 --seed 123 --test_seed 999 --automatic_entropy_tuning True --cuda
-python main.py --env-name pen-v0 --num_steps 10000000 --eval_episodes 30 --eval_freq 40 --seed 12345 --test_seed 999 --automatic_entropy_tuning True 
+python main.py --env-name pen-v0 --num_steps 10000000 --eval_episodes 30 --eval_freq 40 --seed 12345 --test_seed 999 --automatic_entropy_tuning True
